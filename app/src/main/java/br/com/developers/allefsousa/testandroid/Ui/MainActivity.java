bdb4a7853e14b0323d587bb2049e89a9d6d7be1f
Allef sousa <allefsousa_1@hotmail.com>
2018-07-07 20:55:20 -0300

    Estrutura de diretorios

--- app/src/main/java/br/com/developers/allefsousa/testandroid/Ui/MainActivity.java
+++ app/src/main/java/br/com/developers/allefsousa/testandroid/Ui/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.developers.allefsousa.testandroid;
+package br.com.developers.allefsousa.testandroid.Ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.developers.allefsousa.testandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
